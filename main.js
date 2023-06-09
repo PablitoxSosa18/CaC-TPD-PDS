2a5d1cbafc338e9663dfe60008dd04cefb08e8f1
Pablitox Sosa <56353169+PablitoxSosa18@users.noreply.github.com>
2023-06-09 15:05:41 -0300

    updated

--- main.js
+++ main.js
@@ -14,3 +14,31 @@ function closePopup() {
 function popUp(URL) {
     window.open(URL, 'Nombre de la ventana', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=350,height=500,left = 30,top = 50');
 }
+
+///Defino valores de las entradas
+
+const entradaALD = 800;
+const entradaMEN = 500;
+
+///Defino valores de los descuentos de las tarjetas
+
+let master = 0.85;
+let visa = 0.90;
+let american = 0.75;
+let debito = 0.60;
+
+///Defino valores de los descuentos de las tarjetas
+
+let usina1 = 0.90;
+let usina2 = 0.85;
+let teatro = 0.90;
+let movistar = 0.95;
+let rural = 0.90;
+
+///Defino valores de los recargos
+
+let tresCuota = 1.15;
+let seisCuota = 1.18;
+let nueveDuota = 1.21;
+let doceCuota = 1.24;
+let IVA = 1.21;
