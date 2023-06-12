80fc6d9204868abe2cd5771fa7f2ff09df25da6a
PablitoxSosa18 <sosapablodavid@gmail.com>
2023-06-11 23:56:47 -0300

    updated

--- main.js
+++ main.js
@@ -12,7 +12,9 @@ function popUp(URL) {
 }
 
 // Inicializo las variables
-let master, visa, american, debito, usina1, usina2, teatro, movistar, rural, tresCuota, seisCuota, nueveDuota, doceCuota, menor, adulto, imputFull, cantALD, cantMEN, lugar, card, cardPago, cardTipo, totalALD, totalMEN, resumen; 
+let master, visa, american, debito, usina1, usina2, teatro, movistar, rural, tresCuota, seisCuota, nueveDuota, doceCuota, menor, adulto, imputFull, cantALD, cantMEN, lugarFecha, cards, cardsCuota, cardsTipo, totalALD, totalMEN, resumenTICK; 
+
+var resetBTN, comprarBTN, resumenBTN, formulario;
 
 ///Defino valores de las entradas
 
@@ -33,7 +35,7 @@ usina1 = 0.10;
 usina2 = 0.15;
 teatro = 0.10;
 movistar = 0.05;
-rural = 0.10;
+rural = 0.15;
 
 ///Defino valores de los recargos
 
@@ -47,20 +49,48 @@ iva = 1.21;
 
 cantALD = document.querySelector('#entradaALD'); 
 cantMEN = document.querySelector('#entradaMEN');
-lugar = document.querySelector('#lugar');
-card = document.querySelector('#tarjeta');
-cardTipo = document.querySelector('#tipo');
-cardPago = document.querySelector('#pago');
+lugarFecha = document.querySelector('#lugar');
+cards = document.querySelector('#tarjeta');
+cardsTipo = document.querySelector('#tipo');
+cardsCuota = document.querySelector('#pago');
 totalALD = document.querySelector('.totalALD');
 totalMEN = document.querySelector('.totalMEN');
-resumen = document.querySelector('#resumen');
-adulto = parseInt(cantALD.value);
-menor = parseInt(cantMEN.value);
+resumenTICK = document.querySelector('resumenTOTAL');
+resetBTN = document.querySelector('#resetBTN');
+resumenBTN = document.querySelector('#resumenBTN');
+comprarBTN = document.querySelector('#comprarBTN');
+formulario = document.querySelector('#formulario');
 
 ///Funciones
 
+function actualizarSubcategoria() {
+    var tipoSelect = document.getElementById("tipo");
+    var pagoSelect = document.getElementById("pago");
+
+    // Limpiar opciones anteriores
+    pagoSelect.innerHTML = '<option selected>---PAGO---</option>';
+
+    // Obtener la categoría seleccionada
+    var categoriaSeleccionada = tipoSelect.value;
+
+    // Agregar las opciones correspondientes a la categoría seleccionada
+    if (categoriaSeleccionada === "1") {
+      // Opciones para la categoría "Debito"
+      pagoSelect.innerHTML += '<option value="1">UN PAGO</option>';
+    } else if (categoriaSeleccionada === "2") {
+      // Opciones para la categoría "Credito"
+      pagoSelect.innerHTML += '<option value="1">UN PAGO</option>';
+      pagoSelect.innerHTML += '<option value="2">3 CUOTAS</option>';
+      pagoSelect.innerHTML += '<option value="3">6 CUOTAS</option>';
+      pagoSelect.innerHTML += '<option value="4">9 CUOTAS</option>';
+      pagoSelect.innerHTML += '<option value="5">12 CUOTAS</option>';
+  }
+};
+
+cardsTipo.addEventListener("click", (e) => { actualizarSubcategoria();});
+
 let totalSubPagoALD = (cantidad, div) => {
-    div.textContent = `Total: $ ${entradaALD* cantidad}`;
+    div.textContent = `Total: $ ${entradaALD * cantidad}`;
 };
 
 let totalSubPagoMEN = (cantidad, div) => {
@@ -73,5 +103,64 @@ let totalSubPagoMEN = (cantidad, div) => {
 };
 
 cantALD.addEventListener("input", (e) => { totalSubPagoALD(cantALD.value, totalALD);});
-
 cantMEN.addEventListener("input", (e) => { totalSubPagoMEN(cantMEN.value, totalMEN);});
+
+let totalPago = (cantidadALD, cantidadMEN, lugar, tarjeta, tipo, cuota, div) => {
+    var subPagoALD, subPagoMEN, precioTICK, subTotal, subTotalCuotas, parResumen;
+    subPagoALD = entradaALD * cantidadALD;
+    if(cantMEN.value % 2 === 0){
+        subPagoMEN = entradaMEN * cantidadMEN / 2;
+    }
+    else{        
+        subPagoMEN = entradaMEN * cantidadMEN / 2 + entradaMEN / 2;
+    }
+    precioTICK = (subPagoALD + subPagoMEN) * iva;
+    if ( (lugar === 1 || lugar === 2 || lugar === 3 || lugar === 4 ) && ( tarjeta === 1 || tarjeta === 2 || tarjeta === 3 ) && tipo === 1 && cuota === 1) {
+        subTotal = precioTICK * debito + cargo;
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admicion de $ ${cargo}, eligiendo UN PAGO con la tarjeta ${tarjeta} - ${tipo}, se aplico un descuento del ${debito * 100}%. Siendo un monto total de: \$${subTotal}`;
+    } 
+    else if ( lugar === 1 && tarjeta === 1 && tipo === 2 && cuota === 1 ) {
+        subTotal = precioTICK * (visa - usina2);
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admisión de $ ${cargo}, eligiendo UN PAGO con la tarjeta de crédito ${tarjeta}, se aplico un descuento del ${visa * 100}%, mas aplicando un descuento por asistir en ${lugar}, siendo del ${usina2 * 100}%. Con un monto total de: \$${subTotal}`;
+    }
+    else if ( lugar === 1 && tarjeta === 3 && tipo === 2 && cuota === 1 ) {
+        subTotal = precioTICK * (american - usina1) + cargo;
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admisión de $ ${cargo}, eligiendo UN PAGO con la tarjeta de crédito ${tarjeta}, se aplico un descuento del ${american * 100}%, mas aplicando un descuento por asistir en ${lugar}, siendo del ${usina1 * 100}%. Con un monto total de: \$${subTotal}`;
+    }
+    else if ( lugar === 2 && tarjeta === 3 && tipo === 2 && cuota === 1 ) {
+        subTotal = precioTICK * (american - teatro) + cargo;
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admisión de $ ${cargo}, eligiendo UN PAGO con la tarjeta de crédito ${tarjeta}, se aplico un descuento del ${american * 100}%, mas aplicando un descuento por asistir en ${lugar}, siendo del ${teatro * 100}%. Con un monto total de: \$${subTotal}`;
+    }
+    else if ( lugar === 3 && tarjeta === 2 && tipo === 2 && cuota === 1 ) {
+        subTotal = precioTICK * (master - movistar) + cargo;
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admisión de $ ${cargo}, eligiendo UN PAGO con la tarjeta de crédito ${tarjeta}, se aplico un descuento del ${master * 100}%, mas aplicando un descuento por asistir en ${lugar}, siendo del ${movistar * 100}%. Con un monto total de: \$${subTotal}`;
+    }
+    else if ( lugar === 4 && tarjeta === 1 && tipo === 2 && cuota === 1 ) {
+        subTotal = precioTICK * (visa - rural) + cargo;
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admisión de $ ${cargo}, eligiendo UN PAGO con la tarjeta de crédito ${tarjeta}, se aplico un descuento del ${visa * 100}%, mas aplicando un descuento por asistir en ${lugar}, siendo del ${usina2 * 100}%. Con un monto total de: \$${subTotal}`;
+    }
+    else if ( (lugar === 1 || lugar === 2 || lugar === 3 || lugar === 4 ) && ( tarjeta === 1 || tarjeta === 2 || tarjeta === 3 ) && tipo === 2 && cuota === 2){
+        subTotal = precioTICK * tresCuota + cargo;
+        subTotalCuotas = subTotal / tresCuota;
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admisión de $ ${cargo}, eligiendo ${cuota} con la tarjeta de crédito ${tarjeta}, se aplico un descuento del un recargo del ${tresCuota * 100 - 100}%. Siendo un monto total de: \$${subTotal}, pagando en ${cuota} de ${subTotalCuotas}`;
+    }
+    else if ( (lugar === 1 || lugar === 2 || lugar === 3 || lugar === 4 ) && ( tarjeta === 1 || tarjeta === 2 || tarjeta === 3 ) && tipo === 2 && cuota === 3){
+        subTotal = precioTICK * seisCuota + cargo;
+        subTotalCuotas = subTotal / seisCuota;
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admisión de $ ${cargo}, eligiendo ${cuota} con la tarjeta de crédito ${tarjeta}, se aplico un descuento del un recargo del ${seisCuota * 100 - 100}%. Siendo un monto total de: \$${subTotal}, pagando en ${cuota} de ${subTotalCuotas}`;
+    }
+    else if ( (lugar === 1 || lugar === 2 || lugar === 3 || lugar === 4 ) && ( tarjeta === 1 || tarjeta === 2 || tarjeta === 3 ) && tipo === 2 && cuota === 4){
+        subTotal = precioTICK * nueveDuota + cargo;
+        subTotalCuotas = subTotal / nueveDuota;
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admisión de $ ${cargo}, eligiendo ${cuota} con la tarjeta de crédito ${tarjeta}, se aplico un descuento del un recargo del ${nueveDuota * 100 - 100}%. Siendo un monto total de: \$${subTotal}, pagando en ${cuota} de ${subTotalCuotas}`;
+    }
+    else if ( (lugar === 1 || lugar === 2 || lugar === 3 || lugar === 4 ) && ( tarjeta === 1 || tarjeta === 2 || tarjeta === 3 ) && tipo === 2 && cuota === 5){
+        subTotal = precioTICK * doceCuota + cargo;
+        subTotalCuotas = subTotal / doceCuota;
+        div.textContent = `Total de entradas + IVA es: $ ${precioTICK} + cargo de admisión de $ ${cargo}, eligiendo ${cuota} con la tarjeta de crédito ${tarjeta}, se aplico un descuento del un recargo del ${doceCuota * 100 - 100}%. Siendo un monto total de: \$${subTotal}, pagando en ${cuota} de ${subTotalCuotas}`;
+    }
+};
+
+
+resumenBTN.addEventListener("click", (e) => {totalPago(cantALD, cantMEN, lugarFecha, cards, cardsTipo, cardsCuota, resumenTICK);});
+
